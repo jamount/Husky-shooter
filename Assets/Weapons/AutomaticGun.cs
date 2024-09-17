433c2aa48f5539d2af5fb51db6c4fed3f1adbdaf
jamount <mountfordjames27@gmail.com>
2024-09-17 19:05:48 +0100

    automatic gun

--- Assets/Weapons/AutomaticGun.cs
+++ Assets/Weapons/AutomaticGun.cs
@@ -3,19 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BurstGun : Gun
+public class AutomaticGun : Gun
 {
-    [SerializeField]
-    private int burstBulletCount = 3;
-
-    [SerializeField]
-    private float burstFireRate = 0.2f;
-
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
-        Debug.Log("Burst Gun Start");
+        Debug.Log("SemiAutomatic Start");
 
     }
 
@@ -24,39 +18,24 @@ public class BurstGun : Gun
     {
         timer += Time.deltaTime;
 
-        bool shooting = Input.GetKeyDown(KeyCode.Mouse0);
+        bool shooting = Input.GetKey(KeyCode.Mouse0);
 
         if (shooting && timer >= timeBetweenBullets)
         {
-            StartCoroutine(Burst());
-        }
-    }
-
-    private IEnumerator Burst()
-    {
-        for (int bullet = 1; bullet <= burstBulletCount; bullet++)
-        {
-            if (clipAmmo > 0)
+            if(clipAmmo > 0)
             {
                 Debug.Log("shooting");
                 base.Shoot();
-
-                if(bullet != burstBulletCount)
-                {
-                    yield return new WaitForSeconds(burstFireRate);
-                }
             }
             else if (currentAmmo > 0)
             {
                 Debug.Log("reloading");
                 base.Reload();
-                break;
             }
             else
             {
                 Debug.Log("No Ammo");
-
-                break;
+                //handle empty ammo
             }
         }
     }
